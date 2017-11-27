a09f531045cf282edf0ced56aa54de08696721f4
lorenzo <lorynot@gmail.com>
2017-11-27 11:35:56 -0800

    remove directory test

--- test/javatest.java
+++
@@ -1,8 +0,0 @@
-public class javatest{
-	public static void main(String[] args) {
-		long start = System.currentTimeMillis();
-		int max = 100000;
-		for(int i = 0;i<max;i++){}
-		System.out.println("Done in " + (System.currentTimeMillis() - start) + " milliseconds.");
-	}
-}
--- test/pythontest.py
+++
@@ -1,9 +0,0 @@
-#/usr/bin/python
-#run with python, not python3
-from datetime import datetime
-current_milli_time = lambda: datetime.now().microsecond
-start = current_milli_time()
-for x in range(0,100000):
-	pass
-time = float((current_milli_time() - start))
-print("Done in %d milliseconds." % time)
