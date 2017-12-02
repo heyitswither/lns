dccb414cfc7e05f08cd62d71ced0ebb97bd5b2d5
lorenzo <lorynot@gmail.com>
2017-12-01 21:10:18 -0800

    update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 cd ..
-cd source
+
+echo Removing previous versions...
+
+sudo rm -f /usr/bin/lns
+sudo rm -r -f /lns
+
+sudo mkdir --parents /lns/lib
+
 echo Compiling...
-sudo g++ -w --permissive lns.cpp -o ./lns
-echo Installing...
-sudo mv lns /usr/bin/lns
-echo Installing man entry...
-cd ..
-sudo cp manual/lns.1 /usr/share/man/man1/lns.1
-sudo gzip /usr/share/man/man1/lns.1 -f
-echo Installation completed. You can access the manual entry with the command "man lns"
+sudo g++ -w --permissive source/lns.cpp source/errors.h source/exceptions.h source/scanner.h source/defs.h source/parser.h source/options.h source/interpreter.h source/environments.h source/debug.h source/commands.h source/commands.cpp source/debug.cpp source/defs.cpp source/errors.cpp source/exceptions.cpp source/interpreter.cpp source/parser.cpp source/scanner.cpp source/stmt.h source/expr.h -o ./lns
+
+echo Moving executable...
+sudo mv lns /lns/
+
+echo Creating link to executable...
+sudo ln -s /lns/lns /usr/bin/
+
+echo Moving libraries...
+sudo cp -r lib /lns/
+
+echo Done.
