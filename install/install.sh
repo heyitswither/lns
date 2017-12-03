485239fad5a4784fc931922bb2f20418212a3708
lorenzo <lorynot@gmail.com>
2017-12-02 18:12:54 -0800

    Update install.sh
    added option -rdynamic to allow dynamic libraries to access symbols defined in the main executable

--- install/install.sh
+++ install/install.sh
@@ -9,7 +9,7 @@ sudo rm -r -f /lns
 sudo mkdir --parents /lns/lib
 
 echo Compiling...
-sudo g++ -w --permissive source/lns.cpp source/errors.h source/exceptions.h source/scanner.h source/defs.h source/parser.h source/options.h source/interpreter.h source/environments.h source/debug.h source/commands.h source/commands.cpp source/debug.cpp source/defs.cpp source/errors.cpp source/exceptions.cpp source/interpreter.cpp source/parser.cpp source/scanner.cpp source/stmt.h source/expr.h -o ./lns
+sudo g++ -Wall -fpermissive -rdynamic -Wl,--no-as-needed -ldl source/lns.cpp source/errors.h source/exceptions.h source/scanner.h source/defs.h source/parser.h source/options.h source/interpreter.h source/environments.h source/debug.h source/commands.h source/commands.cpp source/debug.cpp source/defs.cpp source/errors.cpp source/exceptions.cpp source/interpreter.cpp source/parser.cpp source/scanner.cpp source/stmt.h source/expr.h -o ./lns
 
 echo Moving executable...
 sudo mv lns /lns/
