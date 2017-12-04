d0e97d8fe72053981cdedc9532808f252ed42f49
lorenzo <lorynot@gmail.com>
2017-12-03 19:10:27 -0800

    add file options.cpp

---
+++ source/options.cpp
@@ -0,0 +1,14 @@
+//
+// Created by lorenzo on 12/3/17.
+//
+
+#include "options.h"
+
+bool lns::direct_execution = false;
+bool lns::silent_full = false;
+bool lns::silent_execution = false;
+bool lns::permissive = false;
+bool lns::time_count = false;
+bool lns::parse_only = false;
+bool lns::prompt = false;
+bool lns::debugger_option = false;
