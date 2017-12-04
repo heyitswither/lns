978767577e5ccd9432127a34a1f34058d0460d3f
lorenzo <lorynot@gmail.com>
2017-12-03 19:13:06 -0800

    add lns_math.cpp natives

---
+++ lib/natives/src/lns_math.cpp
@@ -0,0 +1,240 @@
+//
+// Created by lorenzo on 12/3/17.
+//
+#include "defs.h"
+#include <cmath>
+
+#define S(s) *new std::string(#s)
+
+using namespace lns;
+
+class log10_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(log10);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(log10(nr->value));
+    }
+};
+class log2_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(log2);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(log2(nr->value));
+    }
+};
+class ln_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(ln);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(log(nr->value));
+    }
+};
+class sin_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(sin);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(sin(nr->value));
+    }
+};
+class cos_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(cos);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(cos(nr->value));
+    }
+};
+class tan_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(tan);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(tan(nr->value));
+    }
+};
+class asin_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(asin);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(asin(nr->value));
+    }
+};
+class acos_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(acos);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(acos(nr->value));
+    }
+};
+class atan_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(atan);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(atan(nr->value));
+    }
+};
+class sqrt_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(sqrt);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(sqrt(nr->value));
+    }
+};
+class cbrt_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(cbrt);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(cbrt(nr->value));
+    }
+};
+class ceil_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(ceil);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(ceil(nr->value));
+    }
+};
+class floor_c : public callable{
+public:
+    const int arity() const override {
+        return 1;
+    }
+
+    const std::string &name() const override {
+        return S(floor);
+    }
+
+    object *call(std::vector<object *> &args) override {
+        number_o* nr = dynamic_cast<number_o*>(args[0]);
+        if(nr == nullptr) return new null_o();
+        return new number_o(sqrt(nr->value));
+    }
+};
+
+object* gen(){
+    context* ctx = new context(nullptr);
+    ctx->define(S(log10),new log10_c, true);
+    ctx->define(S(log2),new log2_c, true);
+    ctx->define(S(ln),new ln_c, true);
+    ctx->define(S(sin),new sin_c, true);
+    ctx->define(S(cos),new cos_c, true);
+    ctx->define(S(tan),new tan_c, true);
+    ctx->define(S(asin),new asin_c, true);
+    ctx->define(S(acos),new acos_c, true);
+    ctx->define(S(atan),new atan_c, true);
+    ctx->define(S(sqrt),new sqrt_c, true);
+    ctx->define(S(cbrt),new cbrt_c, true);
+    ctx->define(S(ceil),new ceil_c, true);
+    ctx->define(S(floor),new floor_c, true);
+    return ctx;
+}
+
+extern "C" object* supplier(){
+    return gen();
+}
