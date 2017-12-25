07c233dc470754aed81a2df78128619e2c78609b
lorenzo <lorynot@gmail.com>
2017-12-24 17:14:17 -0800

    method createDescription has initializer_list argument

--- source/exceptions.cpp
+++ source/exceptions.cpp
@@ -5,7 +5,11 @@
 
 #define CMD_USAGE "- lns [script] [options...]"
 using namespace std;
-const char *lns::createDescription(string s) {
+const char *lns::createDescription(initializer_list<string> ss) {
+    string s;
+    for(auto &s1 : ss){
+        s.append(s1);
+    }
     s.append("\n");
     char *ret = (char *) malloc(s.size());
     strcpy(ret, s.c_str());
@@ -13,35 +17,36 @@ const char *lns::createDescription(string s) {
 }
 
 const char *lns::unknown_option_exception::what() const throw(){
-    string s = FATAL_ERROR_S ": unknown option '";
+    string s = FATAL_ERROR_S;
+    s += ": unknown option '";
     s += option;
     s += "'.";
-    return createDescription(s);
+    return createDescription({s});
 }
 
 lns::unknown_option_exception::unknown_option_exception(string o) : option(o) {}
 
 const char *lns::memory_leak_exception::what() const throw(){
-    return createDescription(FATAL_ERROR_S ": a memory leak occurred during execution.");
+    return createDescription({FATAL_ERROR_S,": a memory leak occurred during execution."});
 }
 
 const char *lns::wrong_usage_exception::what() const throw(){
-    return createDescription(
-            FATAL_ERROR_S ": wrong usage. Usage:\n        " CMD_USAGE);
+    return createDescription({
+            FATAL_ERROR_S,": wrong usage. Usage:\n        " CMD_USAGE});
 }
 
 lns::file_not_found_exception::file_not_found_exception(string f) : filename(f) {}
 
 const char *lns::file_not_found_exception::what() const throw(){
-    return createDescription("File " + filename + " doesn't exist or is inaccessible.");
+    return createDescription({"File ", filename," doesn't exist or is inaccessible."});
 }
 
 const char *lns::invalid_operator_exception::what() const throw(){
-    return createDescription("Invalid operator '" + op + "'" + " for type " + type + ".");
+    return createDescription({"Invalid operator '" + op + "'" + " for type " + type + "."});
 }
 
 lns::invalid_operator_exception::invalid_operator_exception(string op, string type) : op(op), type(type) {}
 
 const char *lns::parse_exception::what() const throw(){
-    return createDescription("\nParsing concluded with errors.");
+    return createDescription({"\nParsing concluded with errors."});
 }
