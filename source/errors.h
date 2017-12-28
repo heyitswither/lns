e53bb447b4ef3bd5f41f67882c21b2fbcb8dea94
lorenzo <lorynot@gmail.com>
2017-12-27 22:34:17 -0800

    define macros for scanner and parser error messages

--- source/errors.h
+++ source/errors.h
@@ -13,8 +13,30 @@
 #define RUNTIME_ERROR 3
 #define UNHANDLED_EXCEPTION 4
 
-namespace errors {
 
+/*  SCANNER ERRORS  */
+#define UNTERMINATED(what) "unterminated " what
+#define INVALID_ESCAPE_CHARACTER(c) createDescription({"Invalid escape character '", c, "'"})
+#define UNEXPECTED_CHARACTER(c) createDescription({"Unexpected character '", c, "'"})
+#define UNTERMINATED_BLOCK_COMMENT "unterminated block comment"
+
+/* PARSER ERRORS */
+#define USE_NOT_ALLOWED "'use' statements are not allowed at this location"
+#define EXPECTED(what) "expected " what
+#define EXPECTED_AFTER(what, after) "expected " what " after " after
+#define EXPECTED_IN(what,in) "expected " what " in " in
+#define CAN_ONLY_CONTAIN(a,b) a " can only contain " b
+#define EXPTOCLOSE(what, line) createDescription({"expected 'end' to close ", what ," (opened at line ", std::to_string(line), ")"})
+#define ACCESS_SPEC_NOT_ALLOWED(key,for_) "Access specifier '" key "' is not allowed for " for_ " (--permissive)"
+#define CONSTRUCTOR_OUTSIDE_OF_CLASS "constructor outside of class"
+#define INVALID_STATEMENT "invalid statement"
+#define INVALID_ASSIGNMENT_TARGET "invalid assignment target"
+#define WITHOUT(what,without) what " without " without
+#define MISSING_CLOSING(what) "missing closing " what
+#define USE_FILE_NOT_FOUND "use: file was not found or is inaccessible"
+#define DUPLICATE_ACCESS_SPEC(spec) "duplicate access specifier: '" spec "'"
+#define CONFILICTING_SPECS(a,b) "conflicting access specifiers '" a "' and '" b "'"
+namespace errors {
     extern bool had_parse_error;
     extern int exit_status;
     extern bool had_runtime_error;
