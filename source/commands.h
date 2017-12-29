f2e790b618f3e3728fa5f6f641635e297c90d605
lorenzo <lorynot@gmail.com>
2017-12-28 21:20:14 -0800

    add comments to headers

--- source/commands.h
+++ source/commands.h
@@ -1,6 +1,5 @@
-//
-// Created by lorenzo on 11/8/17.
-//
+/** lns, 2017 */
+
 
 #ifndef CPPLNS_COMMAND_HANDLER_H
 #define CPPLNS_COMMAND_HANDLER_H
@@ -12,12 +11,16 @@
 #include "exceptions.h"
 
 namespace lns {
+
+    /*Required for class command_handler*/
     class command_interpreter;
 
+    /*Enumeration containing the possible actions given to the debugger*/
     enum action {
         CONTINUE_A, STEP_IN, STEP_OUT, STEP_OVER, BREAK_A
     };
 
+    /*Class representing a breakpoint, whose only two properties are the filename and line*/
     class breakpoint {
     public:
         breakpoint(const char *file, int line);
@@ -27,9 +30,12 @@ namespace lns {
     };
 
     class command_handler {
+
+        /*Grant access to methods in this class to comand_interpreter*/
         friend class command_interpreter;
 
     protected:
+
         virtual void load(std::string &filename) = 0;
 
         virtual void run() = 0;
@@ -55,44 +61,66 @@ namespace lns {
         virtual void exit() = 0;
     };
 
+    /*Debugger console handler*/
     class command_interpreter {
     private:
+        /*Pointer to the handler that will execute the commands*/
         command_handler *handler;
+
+        /*used to get one token at a time from user input*/
         std::stringstream &ss;
 
+        /*returns the next token*/
         std::string &advance();
 
+        /*calls advance() and checks if the value is not empty*/
         inline bool has_next();
 
+        /*calls advance(), throws an integer if the end of input has beed reached*/
         std::string &advance_or_throw();
 
+        /* expects the next token to be the last one. Calls advance_or_throw(), then throws an integer
+         * if has_next() returns true */
         std::string &consume();
 
+        /*parses command load*/
         void load();
 
+        /*parses command run*/
         void run();
 
+        /*parses command breakpoint*/
         void breakpoint();
 
-        std::string &line();
-
+        /*parses command watch*/
         void watch();
 
+        /*parses command exit*/
         void exit();
 
+        /*returns the remainder of the current input*/
+        std::string &line();
+
+        /*returns the syntad usage for the given argument*/
         std::string syntax(const std::string &command);
 
     public:
+        /*the pointer to the handler is necessary*/
         command_interpreter() = delete;
 
+        /*only constructor*/
         explicit command_interpreter(command_handler *handler);
 
+        /*prints help*/
         void help();
 
+        /*parses command step*/
         void step();
 
+        /*parses command continue*/
         void continue_();
 
+        /*interprets the input: consumes the first token and calls the right method to parse the rest of the line*/
         void interpret(std::string &input);
     };
 }
--- source/debug.h
+++ source/debug.h
@@ -5,7 +5,7 @@
 #ifndef CPPLNS_DEBUG_H
 #define CPPLNS_DEBUG_H
 
-#define BP_DELIMITER string(":")
+#define BP_DELIMITER string(":") /*The delimiter to be used in breakpoint files*/
 #define MAX_BREAKPOINTS 128
 #define MAX_WATCHES 32
 
@@ -21,7 +21,9 @@ namespace lns {
 
     class watch {
     public:
+        /*The input text to be shown next to the evaluation*/
         string &text;
+        /*The parsed expression*/
         lns::expr *e;
 
         watch() = delete;
@@ -30,62 +32,85 @@ namespace lns {
 
     };
 
+    /*Required for class debug*/
     class debugger;
 
+    /*The debug environment, instanced directly in file lns.cpp*/
     class debug : command_handler {
     private:
+        /*The statements to be executed*/
         vector<stmt *> stmts;
-        default_expr_visitor *visitor;
+        /*The scanner*/
         scanner code_scanner;
+        /*The parser*/
         parser code_parser;
+        /*The command interpreter*/
         command_interpreter command;
+        /*The code interpreter*/
         debugger *interpreter;
+        /*Stores the name of the file given by the user*/
         char *file;
 
+        /*Calls the corresponding method in the debugger*/
         void action(lns::action action) override;
 
+        /*Calls the corresponding method in the debugger*/
         void watch_all() override;
 
+        /*Calls the method loadcode()*/
         void load(string &filename) override;
 
+        /*Calls the corresponding method in the debugger*/
         void watch(string &expr) override;
 
+        /*Begins the execution*/
         void run() override;
 
+        /*Calls the corresponding method in the debugger*/
         void set_break_point(breakpoint *p) override;
 
+        /*Calls the corresponding method in the debugger*/
         void remove_break_point(int id) override;
 
+        /*Calls the corresponding method in the debugger*/
         void list_break_points() override;
 
+        /*Calls the corresponding method in the debugger*/
         void add_watch(string &expression) override;
 
+        /*Calls the corresponding method in the debugger*/
         void remove_watch(int id) override;
 
+        /*Calls the corresponding method in the debugger*/
         void load_break_points(string &filename) override;
 
+        /*Exits the program*/
         void exit() override;
 
+        /*Opens the specified file and returns its content as a string*/
         string &open_file(const char *filename);
 
+        /*Scans and parses the code, stores the statements in 'stmts' and the source code in 'source'*/
         void loadcode(const char *filename);
 
     public:
-        /*INDICATORS*/
+        /*Status indicators*/
         bool broken;
         bool started;
         bool ready;
 
         explicit debug(char *source);
 
+        /* Called by the debugger, enters break mode, prints information about the current breakpoint and
+         * calls broken_loop*/
         void break_(bool stepping, breakpoint *bp, int id);
 
+        /*While 'broken' is true, waits for user input and calls command_interpreter.interpret()*/
         void broken_loop();
 
+        /*Prints the first line to stdout, calls broken_loop()*/
         void start();
 
-        const char *filename;
-        string &source;
     };
 
 
@@ -105,32 +130,46 @@ namespace lns {
 
         explicit debugger(lns::debug *debug);
 
+        /*Checks if the maximum number of breakpoints has been reached, if not adds the new breakpoint*/
         bool set_break_point(breakpoint *p, bool silent);
 
+        /*Removes the breakpoint with the specified index*/
         void remove_break_point(int id);
 
+        /*Lists the current active breakpoints*/
         void list_break_points();
 
+        /*Adds and expression to the watch list*/
         void add_watch(string &expression);
 
+        /*Removes the watch with the specified index*/
         void remove_watch(int id);
 
+        /*Loads breakpoints from a file*/
         void load_break_points(string &filename);
 
+        /*Evaluates an expression and prints its value*/
         void watch(string &str);
 
+        /*Updates the current action and exits the loop in the debug environment*/
         void action(lns::action action);
 
+        /*Executes the statement according to the current action*/
         void execute(stmt *s) override;
 
+        /*Resets all properties of the interpreter*/
         void reset();
 
+        /*Parses an expression*/
         expr *parse_expression(string &str);
 
+        /*Evaluates all the expressions in the watch list and prints their value*/
         void watch_all();
 
+        /*Check if the given statement is targeted by a breakpoint*/
         breakpoint* check_bp(stmt *s);
 
+        /*Returns the length of the stack trace*/
         inline int current_depth();
     };
 
