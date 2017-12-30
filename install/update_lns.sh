bce2ced79abe9cd29eae5183caddc639858cf20a
lorenzo <lorynot@gmail.com>
2017-12-30 15:27:58 -0800

    add file update_lns.sh

---
+++ install/update_lns.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CHK(){
+    if [ $1 -ne 0 ]; then
+        printf "\n"
+        echo "$2. Exiting." & exit 1
+    fi
+}
+
+
+if [ $(which git | wc -l) -eq 0 ]; then
+	echo "Git is not installed. Install it by typing 'sudo apt-get install git'"
+fi
+
+cd ~
+rm -f -r lns_temp
+
+printf "Cloning the repository..."
+git clone https://github.com/lorenzonotaro/lns.git lns_temp
+CHK $? "Unable to clone the repository"
+printf "done.\n"
+
+echo "Calling the install script..."
+bash "lns_temp/install/install.sh"
+
+rm -f -r lns_temp
