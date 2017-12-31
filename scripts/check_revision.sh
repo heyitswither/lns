451c62eb5ae433f28b89a3f283499464dcb29b69
lorenzo <lorynot@gmail.com>
2017-12-30 18:44:08 -0800

    check_revision.sh: remove messages, implement exit codes

--- scripts/check_revision.sh
+++ scripts/check_revision.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+#check if a newer version of lns is available
+#returns:
+#    0 if the interpreter is up to date
+#    1 if an update is available
+#    2 if an error occurred
+
+
 RESULT=$(curl -s https://raw.githubusercontent.com/lorenzonotaro/lns/master/source/revision.h | grep -o "[0-9]*$")
 if [ -z $RESULT ]; then
-    exit 1
+    exit 2
 fi
 
 if [ $1 -lt $RESULT ]; then
-   echo "A newer version of lns is available (rev. $RESULT). Launch lns with the --update option to download it."
+   printf "A newer version of lns is available (rev. $RESULT). "
+   exit 1
 fi
+
+exit 0
