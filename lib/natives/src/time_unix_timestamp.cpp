9a2d0b3023d566ac08d6986920377a546d0a464f
lorenzo <lorynot@gmail.com>
2018-01-07 15:51:06 -0800

    add native library file unix_timestamp.cpp

---
+++ lib/natives/src/time_unix_timestamp.cpp
@@ -0,0 +1,34 @@
+//
+// Created by lorenzo on 12/5/17.
+//
+#include "defs.h"
+#include <string>
+#include <iostream>
+#include <chrono>
+
+using namespace lns;
+using namespace std;
+using namespace chrono;
+
+class time_c : public native_callable {
+public:
+
+    time_c() : native_callable(0) {}
+
+    const std::string name() const override {
+        return std::string("unix_timestamp");
+    }
+
+    shared_ptr <object> call(std::vector<shared_ptr < object>>
+
+    &args)
+
+    override {
+        return make_shared<number_o>(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
+    }
+};
+
+
+extern "C" shared_ptr <object> supplier() {
+    return make_shared<time_c>();
+}
